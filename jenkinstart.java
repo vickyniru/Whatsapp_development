f71755220127215b168cc9892f2b8134020933c2
vickyniru <vickyniru@gmail.com>
2025-01-30 12:02:00 +0100

    Add files via upload
    Initial commit

---
+++ jenkinstart.java
@@ -0,0 +1,11 @@
+public class jenkinstart {
+	
+	 
+
+   public static void main(String[]args) {
+	   System.out.println("Niranjana");
+       System.out.println("Akshatha");
+       System.out.println("Mirudhu");
+       System.out.println("Vignesh");
+}
+}
